e9904aedece9ff795ac33e6d7540c50bb5ffde6a
Luke Joyce <lujo8034@colorado.edu>
2021-04-07 17:06:04 -0600

    Update (lab 5 and project)

--- CSCI3302_Lab5/controllers/lab5_controller/AStar_planner.py
+++ CSCI3302_Lab5/controllers/lab5_controller/AStar_planner.py
@@ -48,48 +48,46 @@ def path_safer(prviousNode, currentNude):
 """	
 
 def a_star_path_paln(map, start, end):
-	start_position = Nodes(None, start)
+    start_position = Nodes(None, start)
 
-	end_position = Nodes(None, end)
+    end_position = Nodes(None, end)
 
-	visted_NodeList = [] #For node n, visted_NodeList[n] is the node immediately preceding it on the cheapest path from start
+    visted_NodeList = [] #For node n, visted_NodeList[n] is the node immediately preceding it on the cheapest path from start
 
-	not_visted_NodeList=PriorityQueue()
+    not_visted_NodeList=PriorityQueue()
 
-	not_visted_NodeList.append(start_position)
+    not_visted_NodeList.append(start_position)
 
-	while len(not_visted_NodeList) > 0:
-		# not_visted_NodeList.sort()
-		current = not_visted_NodeList.pop(0)
-		visted_NodeList.append(current)
+    while len(not_visted_NodeList) > 0:
+        # not_visted_NodeList.sort()
+        current = not_visted_NodeList.pop(0)
+        visted_NodeList.append(current)
 
-		if current == end_position:
-			path =[]
-			while current!= end_position:
-				path.append(current.currentNude)
-				current = current.prviousNode
-			return path[::-1]
+        if current == end_position:
+            path =[]
+            while current!= end_position:
+                path.append(current.currentNude)
+                current = current.prviousNode
+            return path[::-1]
 
-		(x,y) = current.currentNude
+        (x,y) = current.currentNude
 
-		node_neibers.location = [
-		(x-1, y), #up
-		(x+1, y), #down
-		(x, y-1), #left
-		(x, y+1), #right
-		]  
-		for ii in range((node_neibers)):
-			for jj in range(node_neibers):
+        node_neibers.location = [
+        (x-1, y), #up
+        (x+1, y), #down
+        (x, y-1), #left
+        (x, y+1), #right
+        ]  
+        for ii in range((node_neibers)):
+            for jj in range(node_neibers):
 				
-				map_path = map.get(ii),map.get(jj)
+                map_path = map.get(ii),map.get(jj)
+                if (map_path!='0'):
+                    continue
+                add_node_to_list = Nodes(ii, current)
 
-				if (map_path!='0'):
-					continue
-
-				add_node_to_list = Nodes(ii, current)
-
-				if added_node_list in visted_NodeList:
-					pass
+                if added_node_list in visted_NodeList:
+                    pass
 
 
  
@@ -97,24 +95,21 @@ def a_star_path_paln(map, start, end):
 			# add_node_to_list.G =abs(add_node_to_list.currentNude[0] - start_position.currentNude[0])+abs(add_node_to_list.currentNude[0] - start_position.currentNude[0])
 			# add_node_to_list.H =abs(add_node_to_list.currentNude[0] - end_position.currentNude[0])+abs(add_node_to_list.currentNude[0] - end_position.currentNude[0]) 
 			# add_node_to_list.F = add_node_to_list.G + add_node_to_list.H 
-			add_node_to_list.F[start] = heuristic(start_position.get_location(), end_position.get_location())
+        add_node_to_list.F[start] = heuristic(start_position.get_location(), end_position.get_location())
 
-			for node in range(not_visted_NodeList):
-				if (add_node_to_list == node and add_node_to_list.F >= node.F):
-					not_visted_NodeList.append(add_node_to_list)
+        for node in range(not_visted_NodeList):
+            if (add_node_to_list == node and add_node_to_list.F >= node.F):
+                not_visted_NodeList.append(add_node_to_list)
 
-				pass
-		return None
+                    pass
+        return None
 
-
-
-
-	pass
+    pass
 
 def heuristic(point1, point2):
-	x1,y1 = point1
-	x2, y2= point2
-	return abs(x1 - x2) + abs(y1 - y2)
+    x1,y1 = point1
+    x2, y2= point2
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
 
